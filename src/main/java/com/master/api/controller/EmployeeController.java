978ebbba4a46a5f443abeda68fb327f8d8c122d4
Akshay <acksayabhalekar@gmail.com>
2022-11-25 15:50:52 +0530

    I have made PutMapping

--- src/main/java/com/master/api/controller/EmployeeController.java
+++ src/main/java/com/master/api/controller/EmployeeController.java
@@ -1,8 +1,15 @@
 package com.master.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.master.api.entity.Employee;
 import com.master.api.service.EmployeeService;
 
 @RestController
@@ -10,4 +17,11 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService service;
+	
+	@PutMapping
+	public ResponseEntity<?> updateEmp(@RequestBody Employee emp,@PathVariable Long id){
+		this.service.changesEmpDetails(emp,id);
+		return new ResponseEntity<>(HttpStatus.OK);
+		
+	}
 }
--- src/main/java/com/master/api/service/EmployeeServiceImpl.java
+++ src/main/java/com/master/api/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.master.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.master.api.entity.Employee;
 import com.master.api.repo.EmployeeRepo;
 
 @Service
@@ -10,4 +11,23 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
 	private EmployeeRepo repo;
+
+	@Override
+	public void changesEmpDetails(Employee emp, Long id) {
+		Employee emp1 = this.repo.findById(id).orElseThrow();
+		emp1.setId(emp.getId());
+		emp1.setFirstName(emp.getAddress());
+		emp1.setLastName(emp.getLastName());
+		emp1.setGender(emp.getGender());
+		emp1.setAddress(emp.getAddress());
+		emp1.setMobileNo(emp.getMobileNo());
+		emp1.setEmailId(emp.getEmailId());
+		emp1.setCreatedDate(emp.getCreatedDate());
+		emp1.setCreatedtime(emp.getCreatedtime());
+		emp1.setUpdatedDateAndTime(emp.getUpdatedDateAndTime());
+		emp1.setPanCard(emp.getPanCard());
+		emp1.setAdharNumber(emp.getAdharNumber());
+		
+		this.repo.save(emp1);
+	}
 }
