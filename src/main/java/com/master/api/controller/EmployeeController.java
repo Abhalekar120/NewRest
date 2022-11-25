768c6a6d5bc6445e27bdc82a266d4d7efcd170a5
Akshay <acksayabhalekar@gmail.com>
2022-11-25 16:03:24 +0530

    updated

--- src/main/java/com/master/api/controller/EmployeeController.java
+++ src/main/java/com/master/api/controller/EmployeeController.java
@@ -2,7 +2,6 @@ package com.master.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -18,7 +17,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 	
-	@PutMapping
+	@PutMapping("/employee")
 	public ResponseEntity<?> updateEmp(@RequestBody Employee emp,@PathVariable Long id){
 		this.service.changesEmpDetails(emp,id);
 		return new ResponseEntity<>(HttpStatus.OK);
